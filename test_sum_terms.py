066a49870c24d5af2887b16a1bc329e861c1be54
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-10-22 00:19:56 -0700

    Sum of the first nth term tested

---
+++ test_sum_terms.py
@@ -0,0 +1,15 @@
+"""Testing  the  function which returns the sum of following
+series upto nth term(parameter)."""
+import pytest
+
+TEST = [(1, "1.00"),
+        (4, "1.49"),
+        (6, "1.63"),
+        (15, "1.94"),
+        (58, "2.40")]
+
+@pytest.mark.parametrize('input,output',TEST)
+def test_series_sum(input, output):
+    """test for the sum of following nth series"""
+    from sum_terms import  series_sum
+    assert series_sum(input) ==  output
