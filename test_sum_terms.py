734884db84522bc214900e0c0d325d045db3688b
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-10-25 23:56:29 -0700

    Fixed Errors

--- test_sum_terms.py
+++ test_sum_terms.py
@@ -8,8 +8,9 @@ TEST = [(1, "1.00"),
         (15, "1.94"),
         (58, "2.40")]
 
-@pytest.mark.parametrize('input,output',TEST)
+
+@pytest.mark.parametrize('input,output', TEST)
 def test_series_sum(input, output):
     """test for the sum of following nth series"""
-    from sum_terms import  series_sum
-    assert series_sum(input) ==  output
+    from sum_terms import series_sum
+    assert series_sum(input) == output
